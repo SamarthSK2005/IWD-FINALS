c9d981fe3f1933014896b2a3f41663004014703d
SamarthSK2005 <dk2samy@gmail.com>
2024-01-06 12:49:38 +0530

    ef

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ function scrollToTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 }
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+        .then((response) => {
+            msg.innerHTML = "Message sent successfully";
+            setTimeout(function () {
+                msg.innerHTML = "";
+            }, 5000);
+            form.reset();
+        })
+        .catch((error) => console.error("Error!", error.message));
+});
